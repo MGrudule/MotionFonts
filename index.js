0c6e0ecc70e2992c783b5e37d0d3c75e4c1667d5
Maija <hello@maijagrudule.com>
2019-05-06 16:28:59 +0200

    socket setup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-
+const express = require('express'),
+ socket = require('socket.io');
 //App setup
 
 const app = express();
@@ -9,3 +9,10 @@ const server = app.listen(port, () => console.log(`Listening on port ${port}!`))
 //static files
 
 app.use(express.static('public'));
+
+
+//Socket setup
+
+const io = socket(server);
+
+io.on('connection', (socket) => console.log(`made socket connection ${socket.id}`));
